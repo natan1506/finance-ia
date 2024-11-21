99c590bfaf752c3976791f57e10f5e09235322c7
Natan Souza <42328139+natan1506@users.noreply.github.com>
2024-11-21 07:13:49 -0300

    feat: create webhook to events on stripe

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -1,8 +1,6 @@
 import { clerkClient } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
-// import getRawBody from "raw-body";
-// import { NextApiRequest } from "next";
 
 export const config = {
   api: {
@@ -17,14 +15,12 @@ export const POST = async (request: Request) => {
   if (!process.env.STRIPE_SECRET_KEY || !process.env.STRIPE_WEBHOOK_SECRET) {
     return NextResponse.error();
   }
-  // const signature = request.headers["stripe-signature"] as string;
   const signature = request.headers.get("stripe-signature") as string;
   if (!signature) {
     return NextResponse.error();
   }
 
   const text = await request.text();
-  // const text = await getRawBody(request);
 
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
     apiVersion: "2024-10-28.acacia",
